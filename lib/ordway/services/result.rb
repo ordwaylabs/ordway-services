87ec3e22b220aa32ab5e55d21832a57743ca3bc9
Pradeep Kumar <pradeep@surepeople.com>
2020-11-19 14:54:47 +0530

    Added status to result

--- lib/ordway/services/result.rb
+++ lib/ordway/services/result.rb
@@ -1,15 +1,16 @@
 module Ordway
   module Services
     class Result
-      attr_accessor :data, :errors, :warnings, :request_id
+      attr_accessor :data, :errors, :warnings, :request_id, :status
 
       WARNING = 'WARNING'.freeze
 
-      def initialize(request_id)
+      def initialize(request_id, status = :COMPLETED)
         @data = {}
         @errors = []
         @warnings = []
         @request_id = request_id
+        @status = status
       end
 
       def add_warning(message, code = WARNING)
@@ -18,10 +19,6 @@ module Ordway
 
       def add_validation_error(message)
         @errors << Error.new_validation_error(message)
-      end
-
-      def status
-        @errors.blank? ? :COMPLETED : :FAILED
       end
 
       # Add a validation error (if present) and raise an invalid exception
